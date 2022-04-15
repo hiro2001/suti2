e524516b82c3d5c245df17a1fb3c3b4ca2e6ccc6
hiro2001 <hiroyukikorea@gmail.com>
2022-04-15 12:04:00 +0900

    Add

--- 0415-2.c
+++ 0415-2.c
@@ -3,32 +3,29 @@
 #include<math.h>
 
 int main(){
-	int i= 0, m, n[256];
+	int i= 0, m, n[256], total = 0, ave = 0;
 	char A[256];
 	FILE *fp;
 	fp = fopen("date.txt", "r");
-	
 	if(fp == NULL){
 		printf("date.txt file not found.");
 		return 1;
 	}
 	
 	while(fgets(A, 256, fp) != NULL){
-		printf("%s\n", A);
 		n[i] = atoi(A);
 		i++;
 	}
 	
-	printf("%d\n", i);
-
-	printf("hoge\n");
+	printf("格納されていた数字の個数は%d個です。\n", i);
 	m = i;
-
 	for(i = 0; i < m; i++){
 		printf("%d\n", n[i]);
+		total += n[i]
 	}
 	fclose(fp);
-
-
+	ave = total / m;
+	printf("%d", ave);
+	
 	return 0;
 }
