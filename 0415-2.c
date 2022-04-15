51739febc81ec944aed616f5b39b60ccff876d8d
hiro2001 <hiroyukikorea@gmail.com>
2022-04-15 14:10:23 +0900

    ads

--- 0415-2.c
+++ 0415-2.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main(){
-	int i= 0, m, n[256], total = 0, ave = 0;
+	int i= 0, m, n[256];
+	double total = 0, total2 = 0, ave = 0, squ = 0, v = 0, s = 0;
 	char A[256];
 	FILE *fp;
 	fp = fopen("date.txt", "r");
@@ -22,10 +23,17 @@ int main(){
 	for(i = 0; i < m; i++){
 		printf("%d\n", n[i]);
 		total += n[i];
+		total2 += n[i]*n[i];
 	}
 	fclose(fp);
 	ave = total / m;
-	printf("%d", ave);
-	
+	squ = total2 - (total * total / m);
+	v = squ / (m - 1);
+	s = sqrt(v);
+
+	printf("平均：%.3lf\n", ave);
+	printf("分散：%.3lf\n", v);
+	printf("標準偏差：%.3lf\n", s);
+
 	return 0;
 }
