fbaab6d7231e198d9230fab5c15bce587459b114
hiro2001 <hiroyukikorea@gmail.com>
2022-05-24 21:44:56 +0900

    a

---
+++ 0519-3.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+
+int main(){
+	char com1[] = "tudanuma";
+	char com2[] = "tsudanuema";
+	int one[256];
+	int A, B;
+	int c = 0, m = 0, n = 0, i = 0;
+
+	
+	A = strlen(com1);
+	B = strlen(com2);
+	for(m = 0;m < A; m++){
+		printf("%c", com1[m]);
+	}
+	printf("\n");
+	for(m = 0;m < B; m++){
+		printf("%c", com2[m]);
+	}
+	printf("\n");
+	for(i = 0; i < 256; i++)
+		one[i] = 0x0a;
+		
+  i = 0;
+	n = 0;
+	m = 0;	
+	while(1){
+
+		printf("%d  %c\n", i, com2[0]);
+
+		if(com1[m] == com2[n]){
+			one[i] = 1;
+			m++;
+			n++;
+			i++;
+		}
+		else if(com1[m] == com2[n+1]){
+			one[i] = 0;
+			one[i+1] = 1;
+			for(c = A+1; c > i; c--){
+				com1[c] = com1[c - 1];
+			}
+			com1[i] = 0x20;
+			A++;
+			i += 2;
+			m += 2;
+			n += 2;
+		}
+		else if(com1[m+1] == com2[n]){
+			one[i] = 0;
+			one[i+1] = 1;
+			for(c = B+1; c > i; c--){
+				com2[c] = com2[c - 1];
+			}
+			com2[i] = 0x20;
+			B++;
+			i += 2;
+			m += 2;
+			n += 2;
+		}
+		else{
+			one[i] = 0;
+			i++;
+			m++;
+			n++;
+		}
+			
+		if(m >= A && n >= B){
+			break;
+		}
+	}
+	
+	for(m = 0;m < A; m++){
+		printf("%c", com1[m]);
+	}
+	printf("\n");
+	for(m = 0;m < B; m++){
+		printf("%c", com2[m]);
+	}
+	printf("\n");
+	for(m = 0;m < i; m++){
+		switch(one[m]){
+			case 0:
+				printf("x");
+				break;
+			case 1:
+				printf("o");
+				break;
+			default:
+				printf("ellor\n");
+				exit(0);
+				break;
+		}
+	}
+	printf("\n");
+
+	
+	return 0;
+}
